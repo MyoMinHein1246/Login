3249870e1ee0321a80f898511cf1c5b7c0791748
Freddy <myominhein1246@gmail.com>
2023-02-06 17:10:14 +0630

    Fully working login

--- Login.py
+++ Login.py
@@ -6,20 +6,25 @@ import sys
 FILE_NAME = "database.txt"
 
 
-class LoginForm(QDialog):
-	def __init__(self) -> None:
-		super(LoginForm, self).__init__()
-		uic.loadUi("LoginForm.ui", self)
-		self.btnLogin.clicked.connect(self.tryLogin)
-		self.btnCreate.clicked.connect(self.createNewAccount)
-		self.btnClose.clicked.connect(self.exit)
+class MsgBox():
+	def show(self, icon, title: str, msg:str):
+		msgBox = QMessageBox(icon,title, msg)
+		msgBox.setInformativeText("Please try again.")
+		msgBox.exec()
+		print(msg)
 
-	def tryLogin(self):
+
+class LoginCheck():
+	def __init__(self, parent) -> None:
+		self.editUsername = parent.editUsername
+		self.editPassword = parent.editPassword
+
+	def tryLogin(self) -> str:
 		username = self.editUsername.text()
 		password = self.editPassword.text()
 
 		dataDict = self.getDataDict()
-		print(self.login(username, password, dataDict))
+		return self.login(username, password, dataDict)
 
 	def login(self, username: str, password: str, dataDict: list) -> str:
 		msg = "Could not fine any user with the username."
@@ -28,8 +33,8 @@ class LoginForm(QDialog):
 			msg = "Could not fine any user with the username."
 			return msg
 		for data in dataDict:
-			if data['username'] == username.strip():
-				if data['password'] == password.strip():
+			if data['username'] == username:
+				if data['password'] == password:
 					msg = "Login success!"
 					break
 				else:
@@ -38,18 +43,25 @@ class LoginForm(QDialog):
 
 		return msg
 
-	def createNewAccount(self):
+	def createNewAccount(self) -> str:
 		username = self.editUsername.text()
 		password = self.editPassword.text()
+
+		msg = ""
+
+		if self.checkUsername(username):
+			msg = "User already existed with the username."
+			return
 
 		with open(FILE_NAME, 'a') as f:
 			try:
 				f.write("\n{}, {}".format(username, password))
+				msg = "User created successfully!"
 			except Exception:
-				print("Error while writing data.")
+				msg = "Error while writing data."
 				return
 			
-			print("User created successfully!")
+		return msg
 
 	def getDataDict(self) -> list:
 		dataDict = list(dict())
@@ -63,6 +75,30 @@ class LoginForm(QDialog):
 
 		return dataDict
 
+	def checkUsername(self, username: str, dataDict:list = list()) -> bool:
+		if len(dataDict) == 0:
+			dataDict = self.getDataDict()
+
+		for data in dataDict:
+			if data['username'] == username.strip():
+				return True
+
+		return False
+
+
+class LoginForm(QDialog):
+	def __init__(self) -> None:
+		super(LoginForm, self).__init__()
+		uic.loadUi("LoginForm.ui", self)
+		
+		self.msgBox = MsgBox()
+
+		loginCheck = LoginCheck(self)
+
+		self.btnLogin.clicked.connect(lambda: self.msgBox.show(QMessageBox.Information, "Login", loginCheck.tryLogin()))
+		self.btnCreate.clicked.connect(lambda: self.msgBox.show(QMessageBox.Information, "Create", loginCheck.createNewAccount()))
+		self.btnClose.clicked.connect(self.exit)
+		
 	def exit(self):
 		self.close()
 
@@ -71,7 +107,7 @@ def main():
 	app = QApplication.instance()
 	if not app:
 		app = QApplication([])
-
+		
 	form = LoginForm()
 	form.show()
 	sys.exit(app.exec_())
