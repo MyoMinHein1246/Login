1014b33220dbfe0cfa3b50469f0d8a1d0f4ca2e8
Freddy <myominhein1246@gmail.com>
2023-02-11 11:50:33 +0630

    code cleanup

--- Login.py
+++ Login.py
@@ -17,130 +17,139 @@ def resource_path(relative_path) -> str:
     return os.path.join(base_path, relative_path)
 
 
+class MsgBox:
+    def show(self, icon, title: str, msg: str):
+        msgBox = QMessageBox(icon, title, msg)
+        msgBox.exec()
+        print(msg)
 
-class MsgBox():
-	def show(self, icon, title: str, msg:str):
-		msgBox = QMessageBox(icon,title, msg)
-		msgBox.exec()
-		print(msg)
 
+class LoginCheck:
+    def __init__(self, parent, cursor) -> None:
+        self.editUsername = parent.editUsername
+        self.editPassword = parent.editPassword
+        self.cursor = cursor
 
-class LoginCheck():
-	def __init__(self, parent, cursor) -> None:
-		self.editUsername = parent.editUsername
-		self.editPassword = parent.editPassword
-		self.cursor = cursor
+    def tryLogin(self) -> str:
+        username = self.editUsername.text()
+        password = self.editPassword.text()
 
-	def tryLogin(self) -> str:
-		username = self.editUsername.text()
-		password = self.editPassword.text()
+        dataDict = self.getDataDict()
+        return self.login(username, password, dataDict)
 
-		dataDict = self.getDataDict()
-		return self.login(username, password, dataDict)
+    def login(self, username: str, password: str, dataDict: list) -> str:
+        msg = "Could not fine any user with the username."
 
-	def login(self, username: str, password: str, dataDict: list) -> str:
-		msg = "Could not fine any user with the username."
-		
-		if not dataDict:
-			msg = "Could not fine any user with the username."
-			return msg
-		for data in dataDict:
-			if data['username'] == username:
-				if data['password'] == password:
-					msg = "Login success!"
-					break
-				else:
-					msg = "Wrong password!"
-					break
+        if not dataDict:
+            msg = "Could not fine any user with the username."
+            return msg
+        for data in dataDict:
+            if data["username"] == username:
+                if data["password"] == password:
+                    msg = "Login success!"
+                else:
+                    msg = "Wrong password!"
 
-		return msg
+                break
 
-	def createNewAccount(self) -> str:
-		username = self.editUsername.text()
-		password = self.editPassword.text()
+        return msg
 
-		msg = ""
+    def createNewAccount(self) -> str:
+        username = self.editUsername.text()
+        password = self.editPassword.text()
 
-		if self.checkUsername(username):
-			msg = "User already existed with the username."
-			return msg
+        msg = ""
 
-		try:
-			self.cursor.execute("INSERT INTO login(username, password) VALUES(?, ?);", (username, password))
-			msg = "User created successfully!"
-		except Exception:
-			msg = "Error while writing data."
-		finally:
-			return msg
+        if self.checkUsername(username):
+            msg = "User already existed with the username."
+            return msg
 
-	def getDataDict(self) -> list:
-		dataDict = list(dict())
+        try:
+            self.cursor.execute(
+                "INSERT INTO login(username, password) VALUES(?, ?);",
+                (username, password),
+            )
+            msg = "User created successfully!"
+        except Exception:
+            msg = "Error while writing data."
+        finally:
+            return msg
 
-		self.cursor.execute("SELECT * FROM login;")
-		
-		for data in self.cursor.fetchall():
-			id = data[0]
-			username = data[1].strip()
-			password = data[2].strip()
-			data = { 'id': id, 'username':username, 'password':password }
-			dataDict.append(data)
+    def getDataDict(self) -> list:
+        dataDict = list(dict())
 
-		return dataDict
+        self.cursor.execute("SELECT * FROM login;")
 
-	def checkUsername(self, username: str, dataDict:list = list()) -> bool:
-		if len(dataDict) == 0:
-			dataDict = self.getDataDict()
+        for data in self.cursor.fetchall():
+            id = data[0]
+            username = data[1].strip()
+            password = data[2].strip()
+            data = {"id": id, "username": username, "password": password}
+            dataDict.append(data)
 
-		for data in dataDict:
-			if data['username'] == username.strip():
-				return True
+        return dataDict
 
-		return False
+    def checkUsername(self, username: str, dataDict: list = list()) -> bool:
+        if len(dataDict) == 0:
+            dataDict = self.getDataDict()
+
+        for data in dataDict:
+            if data["username"] == username.strip():
+                return True
+
+        return False
 
 
 class LoginForm(QDialog):
-	def __init__(self, cursor) -> None:
-		super(LoginForm, self).__init__()
-		uic.loadUi(resource_path("./Resources/LoginForm.ui"), self)
-		
-		self.msgBox = MsgBox()
+    def __init__(self, cursor) -> None:
+        super(LoginForm, self).__init__()
+        uic.loadUi(resource_path("./Resources/LoginForm.ui"), self)
 
-		loginCheck = LoginCheck(self, cursor)
+        self.msgBox = MsgBox()
 
-		self.btnLogin.clicked.connect(lambda: self.msgBox.show(QMessageBox.Information, "Login", loginCheck.tryLogin()))
-		self.btnCreate.clicked.connect(lambda: self.msgBox.show(QMessageBox.Information, "Create", loginCheck.createNewAccount()))
-		self.btnClose.clicked.connect(self.exit)
-		
-	def exit(self):
-		self.close()
+        loginCheck = LoginCheck(self, cursor)
+
+        self.btnLogin.clicked.connect(
+            lambda: self.msgBox.show(
+                QMessageBox.Information, "Login", loginCheck.tryLogin()
+            )
+        )
+        self.btnCreate.clicked.connect(
+            lambda: self.msgBox.show(
+                QMessageBox.Information, "Create", loginCheck.createNewAccount()
+            )
+        )
+        self.btnClose.clicked.connect(self.exit)
+
+    def exit(self):
+        self.close()
 
 
 def main():
-	try:
-		app = QApplication.instance()
-		if not app:
-			app = QApplication([])
-		
-		connection = sqlite3.connect(resource_path("./Resources/LoginData.db"))
-		cursor = connection.cursor()
+    try:
+        app = QApplication.instance()
+        if not app:
+            app = QApplication([])
 
-		form = LoginForm(cursor)
-		form.show()
+        connection = sqlite3.connect(resource_path("./Resources/LoginData.db"))
+        cursor = connection.cursor()
 
-		code = app.exec_()
+        form = LoginForm(cursor)
+        form.show()
 
-	except sqlite3.Error as err:
-		print(err)
+        code = app.exec_()
 
-	finally:
-		if cursor:
-			connection.commit()
-			cursor.close()
-			connection.close()
+    except sqlite3.Error as err:
+        print(err)
 
-		sys.exit(code)
+    finally:
+        if cursor:
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+        sys.exit(code)
 
 
 if __name__ == "__main__":
-	main()
-
+    main()
